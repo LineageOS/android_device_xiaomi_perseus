14304d595ca6c9d0da4be81f0e6fb6c8af93c076
Christian Oder <myself5@carbonrom.org>
2018-06-27 21:14:43 -0400

    perseus: build disabled vbmeta image
    Change-Id: I381feef8f6fefc8449ca50d85d704b67bcc8a77e

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -38,3 +38,7 @@ TARGET_USES_NQ_NFC := true
 
 # Sepolicy
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
+
+# Verified Boot
+BOARD_AVB_ENABLE := true
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flag 2
