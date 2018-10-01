fca0b0360edc74c5d476c623b4e58fd794e3a858
1nfrag <sil3ntcll@gmail.com>
2018-10-01 21:45:57 +0800

    perseus: Import and bind mount stock manifest
     * Stock vendor manifest from MIUI 9.1.10
    
    Change-Id: I6c779f62aeee393d75b68f4e5be7029ff7eff9c8

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -27,3 +27,6 @@ TARGET_USES_NQ_NFC := true
 
 # Inherit from the proprietary version
 -include vendor/xiaomi/perseus/BoardConfigVendor.mk
+
+# Sepolicy
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
