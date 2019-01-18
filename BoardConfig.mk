9aea0a0a7af0bbd0ecbea309d95b616f6dd1b8c0
Anton Hansson <hansson@google.com>
2019-01-18 14:10:45 +0000

    perseus: Use TARGET_SCREEN_DENSITY
    This variable is used to auto-populate the ro.sf.lcd_density as
    well as selecting density bucket for resources in recovery.
    
    Bug: 118676832
    Test: adb shell getprop ro.sf.lcd_density
    Test: boot into recovery
    Change-Id: Ib383d18b61e7a2058a69ae26136aa053fdba4df2

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,6 +12,9 @@ DEVICE_PATH := device/xiaomi/perseus
 # Assert
 TARGET_OTA_ASSERT_DEVICE := perseus
 
+# Display
+TARGET_SCREEN_DENSITY := 440
+
 # Kernel
 BOARD_KERNEL_SEPARATED_DTBO := true
 TARGET_KERNEL_CONFIG := perseus_defconfig
