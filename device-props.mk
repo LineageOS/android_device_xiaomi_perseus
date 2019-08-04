0e3d8da29185d29d0893d1a94e9ff4947a5ccd5f
Bruno Martins <bgcngm@gmail.com>
2019-08-04 19:26:20 +0100

    perseus: Set device stock props
    Change-Id: Ibcd8df1c8a6c4e8d030815ecde49cd5cacb4aede

--- device-props.mk
+++ device-props.mk
@@ -1,3 +1,7 @@
+# Audio
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.audio.soundfx.type=mi
+
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.camera.remosaic.trigeriso=250 \
@@ -11,9 +15,21 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.sf.lcd_density=440
 
-# Display features
+# Display postprocessing
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.displayfeature.histogram.enable=false \
-    ro.eyecare.brightness.threshold=7 \
-    ro.eyecare.brightness.level=5 \
-    ro.hist.brightness.threshold=7
+    vendor.display.enable_default_color_mode=0
+
+# Fingerprint
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hardware.fp.fpc=true \
+    ro.hardware.fp.goodix=true
+
+# GNSS
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hardware.flp=brcm \
+    ro.hardware.gps=brcm
+
+# NFC
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hardware.nfc_nci=nqx.default \
+    ro.se.type=HCE,UICC
