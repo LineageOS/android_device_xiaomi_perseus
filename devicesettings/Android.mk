47b1934bd23048357e565a1ad72b82d52b39a88d
Nicholas Magill <nmagill1979@gmail.com>
2023-02-22 15:15:42 +0700

    perseus: Add a settings page
    Icons courtesy of https://materialdesignicons.com/
    
    Change-Id: Ifc93a1ae0451547b801da93de58612592c28e255

--- devicesettings/Android.mk
+++ devicesettings/Android.mk
@@ -26,6 +26,14 @@ LOCAL_PRIVATE_PLATFORM_APIS := true
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_MODULE_TAGS := optional
 
+LOCAL_STATIC_ANDROID_LIBRARIES := \
+    androidx.core_core \
+    androidx.preference_preference
+
+LOCAL_RESOURCE_DIR := \
+    $(LOCAL_PATH)/res \
+    $(TOP)/packages/resources/devicesettings/res
+
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
 include $(BUILD_PACKAGE)
