8fe9dc1c93c23f762678c368f482e8a49daa80a4
Tim Zimmermann <tim@linux4.de>
2024-06-04 03:20:41 +0700

    perseus: Inherit non_ab_device.mk
     * This is required to build applypatch for updating recovery
    
    Change-Id: I38053691f7724b5d6c30159c410d5d04ac136180

--- device.mk
+++ device.mk
@@ -4,6 +4,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+$(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
+
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
 # Get non-open-source specific aspects
